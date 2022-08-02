c6bbb6dced3bc961aa14d806f9cb2cbb1e16799e
idris01 <idrys01@gmail.com>
2022-08-03 00:21:56 +0100

    update update infrastructure env variable

--- infrastructure/update_inventory.sh
+++ infrastructure/update_inventory.sh
@@ -3,6 +3,7 @@
 
 aws ec2 describe-instances \
 	--query 'Reservations[*].Instances[*].PublicIpAddress' \
+	--profile default --region us-east-1 \
 	--output text > inventory.txt
 
 export WEB="$(cat inventory.txt)"
